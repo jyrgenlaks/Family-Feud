beba7c6d59feb1f37a2dadd2b2fd37d01c08df7d
Kristjan Laht <kristjan_laht@hotmail.com>
2015-09-30 00:04:07 +0300

    Using getAnimationFrame to draw updates on canvas

--- familyfeud/app.ts
+++ familyfeud/app.ts
@@ -13,11 +13,11 @@ function editCanvas(canvas: HTMLCanvasElement, id:Number) {
     // display video
     ctx.font = "56px Bebas Neue";
     ctx.fillStyle = "white";
-    ctx.fillText("Hello world", 10, 50);
 
-    var drawElement: HTMLVideoElement = media.vid["anim" + id];
- 
-    //setTimeout(animateTurnToText(ctx, id), 5000);
+
+    var drawElement: HTMLVideoElement = media.img[id + "-0"];
+    ctx.drawImage(drawElement, 0, 0);
+    setTimeout(animateTurnToText(ctx, id), 8000);
 }
 
 function playSound(soundType:String) {
@@ -26,9 +26,12 @@ function playSound(soundType:String) {
 
 function animateTurnToText(ctx: CanvasRenderingContext2D, id: Number) {
     var animation: HTMLVideoElement = media.vid["anim" + id];
+    var background: HTMLImageElement = media.img["background"];
     //var finish: HTMLImageElement = media.img[id + "-1"];
 
     var startTime = null;
+
+    
 
     playSound("clang");
     animation.play();
@@ -38,13 +41,17 @@ function animateTurnToText(ctx: CanvasRenderingContext2D, id: Number) {
         if (!startTime) {
             startTime = timestamp;
         }
+        console.log("step");
         var progress = timestamp - startTime;
+        // TODO: make canvas transparent before drawing new frame
+        // TODO: throw videos out of canvas and show them in video tags, to get HW acc
+        //ctx.drawImage(background, 0, 0);
         ctx.drawImage(animation, 0, 0);
-        if (progress < 600) {
+        if (progress < 2000) {
             window.requestAnimationFrame(step);
         }
     }
-    
+    window.requestAnimationFrame(step);
 
 
 }
@@ -56,8 +63,12 @@ function mapClassToId(map, classname: string) {
     });
 }
 
-window.onload = () => {
+function unloadScrollBars() {
+    document.documentElement.style.overflow = 'hidden';  // firefox, chrome
+}
 
+window.onload = () => {
+    unloadScrollBars();
     var canvases = document.getElementsByClassName('vastus');
     
     // map media to their ids
