42a339f934f52a3219ff13fc72c4709d11b7d4bc
Jürgen Laks <jyrgenlaks@gmail.com>
2018-06-18 17:15:30 +0300

    Added theme music functionality, changed the logo and some smaller fixes/changes

--- familyfeud/app.js
+++ familyfeud/app.js
@@ -1,13 +1,44 @@
 /// <reference path="Scripts\typings\lodash\lodash.d.ts" />
+var GAME_BLOCK_NUMBER = 2;
 var canvas;
 var ctx;
 var videos;
-var answersRounds1 = [
+var answersRounds;
+var gameBlock = [];
+
+gameBlock[0] = [
+    [ // Milline on enim kasutatud programmeerimis-/skriptimis-/markupkeel (2018)
+        ["JavaScript", 70],
+        ["HTML", 69],
+        ["CSS", 65],
+        ["SQL", 57]
+    ],
+    [ // Kus õpid programmeerima?
+        ["Õpid ise", 99],
+        ["Ülikoolis", 70],
+        ["Tööl kursustel", 40],
+        ["Netikursustel", 20]
+    ],
+    [ // Milline on populaarseim operatsioonisüsteem? (2018)
+        ["Android", 40],
+        ["Windows", 36],
+        ["iOS", 13],
+        ["OS X", 5]
+    ],
+    [ // Kumb on parem, tabid või tühikud?
+        ["Tabid", 99],
+        ["Tühikud", 50],
+        ["Oleneb", 30],
+        ["Ei tea", 0]
+    ]
+];
+
+gameBlock[1] = [
     [
-        ['M. Reemann', 32],
-        ['T. Jürgenstein', 25],
-        ['E. Paabo', 8],
-        ['P. Beier', 7]
+        ['15 tähte jrlaks', 32],
+        ['20 tähte on siin ses 20 tähte on siin ses hahaa 20 tähte on siin ses 20 tähte on siin ses', 25],
+        ['25 tähte on selles kastis', 8],
+        ['IT ehk perfolintide vahetaja', 7]
     ], [
         ['M. Jõgiste', 16],
         ['M. Piirimäe', 11],
@@ -30,38 +61,57 @@ var answersRounds1 = [
         ['T. Pluum', 10]
     ]
 ];
-var answersRounds = [
-    [
-        ["JavaScript", 99],
-        ["Java", 70],
-        ["Ruby", 40],
-        ["PHP", 20]
-    ],
-    [
-        ["Õpid ise", 99],
-        ["Ülikoolis", 70],
-        ["Tööl kursustel", 40],
-        ["Netikursustel", 20]
-    ],
-    [
-        ["Windows 7", 99],
-        ["Mac OS X", 70],
-        ["Linux", 40],
-        ["Windows 8", 20]
-    ],
-    [
-        ["NotePad++", 99],
-        ["Sublime Text", 70],
-        ["Vim", 40],
-        ["Emacs", 20]
-    ],
-    [
-        ["Tabid", 99],
-        ["Tühikud", 50],
-        ["Oleneb", 30],
-        ["Ei tea", 0]
+
+gameBlock[2] = [
+    [//Nimeta üks Tartu Ülikooli hoone, kust leiab kõige rohkem teadust.
+        ['Füüsikum', 32],
+        ['Keemikum', 25],
+        ['Liivi 2', 8],
+        ['Tü raamatukogu', 7]
+    ], [//Nimeta üks amet, mida vanavanemad enamasti õppima soovitavad minna.
+        ['Aadrilaskja/tohtrionu', 16], // arst
+        ['Perfolindi asetaja/asendaja', 11], // IT
+        ['Insener', 10],
+        ['Põllumajandus/traktorist', 9]
+    ], [//Peale toidu, nimeta veel üks asi, millega saab tudengit loengusse meelitada.
+        ['Loengupunktid', 16],
+        ['Hea õppejõud', 11],
+        ['Meemid/huvitav loeng', 10],
+        ['KT/eksami vastused', 9]
+    ], [//Nimeta toit, mida tudengid tarbivad.
+        ['Šokolaad/snickers', 16],
+        ['(kiir)nuudlid', 11],
+        ['Pelmeenid', 10],
+        ['Purgisupp', 9]
+    ], [//Nimeta üks avalikult tuntud TÜ õppejõud, kes sinu arvates saab kõige rohkem armastuskirju.
+        ['Tarkpea', 16],
+        ['Karek Kolk', 11],
+        ['Mihhail Lotman', 10],
+        ['Mats Mikkor', 9]
+    ], [//Nimeta üks põhjus, miks inimene tahab füüsikuks saada.
+        ['Keskmine palk', 16], // raha
+        ['Masohhistlikult vastupidav', 11],
+        ['Teadmisrõõm', 10],
+        ['Vereliin - pereliin', 9]
+    ], [//Nimeta üks asi, mille üle varas ei tahaks üllatuda, kui ta Füüsika Instituuti sisse murrab?
+        ['Kalev Tarkpea ootab koridoris', 16],
+        ['Tudengeid õppimas', 11],
+        ['STEM kaalub samapalju kui bemm', 10],
+        [' - ', 9]
+    ], [//Nimeta midagi, mida eeldatakse, et 25-aastane inimene on juba saavutanud.
+        ['Pesast välja hüpanud', 16],//Kodust välja kolinud
+        ['Elukaaslane/koduloom', 11],
+        ['Vumm-vumm', 10],//Auto
+        [' - ', 9]
+    ], [//Nimeta miski, mis paneb õppejõude kõige rohkem nutma kui nad eksamit parandavad.
+        ['Magistrisse minna', 16],
+        ['-', 11],
+        ['-', 10],
+        [' - ', 9]
     ]
 ];
+
+
 var turnedBox = [
     false, false, false, false
 ];
@@ -157,6 +207,7 @@ function start() {
     if (startUsed === true) {
         return;
     }
+    answersRounds = gameBlock[GAME_BLOCK_NUMBER];
     var logo = document.getElementById("logo");
     logo.style.display = "none";
     animate("beginning", 1000);
@@ -179,14 +230,50 @@ function turnText(boxId) {
                 pointsSteal();
                 noAdd = true;
             }
-            ctx.font = "200px Bebas Neue";
+
             ctx.fillStyle = "white";
             ctx.shadowColor = "black";
             ctx.shadowBlur = 10;
             ctx.shadowOffsetX = 5;
             ctx.shadowOffsetY = 5;
             ctx.clearRect(boxLocations[boxId * 2 + 0], boxLocations[boxId * 2 + 1] - 300, 850, 350);
-            ctx.fillText(answersRounds[curRound][boxId][0], boxLocations[boxId * 2 + 0], boxLocations[boxId * 2 + 1], 650);
+
+            var strlen = answersRounds[curRound][boxId][0].length;
+            if(strlen < 20){
+            	ctx.font = "200px Bebas Neue";
+            	ctx.fillText(answersRounds[curRound][boxId][0], boxLocations[boxId * 2 + 0], boxLocations[boxId * 2 + 1], 650);
+            }else if(strlen < 40){
+            	ctx.font = "100px Bebas Neue";
+            	ctx.fillText(answersRounds[curRound][boxId][0], boxLocations[boxId * 2 + 0], boxLocations[boxId * 2 + 1] - (200-100)/3, 650);
+            }else{
+            	ctx.font = "100px Bebas Neue";
+            	var str = answersRounds[curRound][boxId][0];
+            	var half = strlen/2;
+            	while(half < strlen){
+            		if(str.charAt(half) == " "){
+            			break;
+            		}else{
+            			half++;
+            		}
+            	}
+            	
+            	var line1 = "esimene asd asd asd asd asd asd";
+            	var line2 = "teine asd asd asd asd asd asd";
+				if(half >= strlen){
+            		//jagame julmalt pooleks
+            		line1 = str.substring(0, strlen/2);
+            		line2 = str.substring(strlen/2, strlen);
+            	}else{
+            		//jagame ilusasti
+            		line1 = str.substring(0, half);
+            		line2 = str.substring(half+1, strlen);
+            	}
+
+				ctx.fillText(line1, boxLocations[boxId * 2 + 0], boxLocations[boxId * 2 + 1] - 100 + 15, 650);
+				ctx.fillText(line2, boxLocations[boxId * 2 + 0], boxLocations[boxId * 2 + 1] - 0   + 15, 650);
+            }
+
+            ctx.font = "200px Bebas Neue";
             ctx.fillText(answersRounds[curRound][boxId][1], numLocations[boxId * 2], numLocations[boxId * 2 + 1]);
             drawLowerText(curTeamScore[0], curTeamScore[1]);
         }, 450);
@@ -215,6 +302,15 @@ function wrongAnswerTeams() {
     else if (curWrongAnswer[curTeam] === 3 && 3) {
         switchTeam();
         curWrongAnswer[curTeam] = 2;
+    }
+}
+function toggleMusic() {
+    audioEl = document.getElementById("theme");
+    if (audioEl.paused) {
+        audioEl.play();
+    } else {
+        audioEl.pause();
+        audioEl.currentTime = 0;
     }
 }
 function selectTeam(team) {
@@ -277,6 +373,9 @@ window.addEventListener("keypress", function (event) {
     }
     else if (event.charCode == 48) {
         noAdd = true;
+    }
+    else if (event.charCode == 109) { // m
+        toggleMusic();
     }
     else {
         console.log(event.charCode);
