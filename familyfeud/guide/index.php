a1b35c7bfb9cd63613c9a4f81da0c57d50f23f5a
Jürgen Laks <jyrgenlaks@gmail.com>
2018-06-18 17:15:14 +0300

    Added a basic webpage that has the questions and answers and also a light-weight guide on which buttons to press for controlling the system

---
+++ familyfeud/guide/index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FamilyFeud Guide</title>
+</head>
+<body>
+	<h1>Family Feud Guide</h1>
+	<li>Ava veebileht</li>
+	<li>Mängu alustamiseks vajuta 'S' (nagu Start)</li>
+	<ul>
+		<li>Alustava tiimi saab valida klahvidega 'J' (vasak) ja 'K' (parem)</li>
+		<li>Seejärel avad kaste numbritega 1, 2, 3 ja 4</li>
+		<li>Vale vastuse korral vajutad 'Q'</li>
+		<li>Uue roundi alustamiseks vajutada 'R' (new Round)</li>
+
+	</ul>
+	Kui üks tiim on 3 korda valesti arvanud, siis käib loogika samamoodi: Q on vale vastus ja numbriklahv märgib õige vastuse
+	<br/><br/>
+	<pre>
+	Deprecated:
+	5, 6, 7 - näitavad vasatavalt 1, 2 või 3 risti
+
+	</pre>
+
+	<h1>Testküsitluse vastused</h1>
+	<pre>
+gameBlock[2] = [
+    [//Nimeta üks Tartu Ülikooli hoone, kust leiab kõige rohkem teadust.
+        ['Füüsikum', 18],
+        ['Keemikum', 12],
+        ['Liivi 2', 3],
+        ['Tü raamatukogu', 1]
+    ], [//Nimeta üks amet, mida vanavanemad enamasti õppima soovitavad minna.
+        ['Aadrilaskja/tohtrionu', 16], // arst
+        ['Perfolindi asetaja/asendaja', 11], // IT
+        ['Insener', 10],
+        ['Põllumajandus/traktorist', 6]
+    ], [//Peale toidu, nimeta veel üks asi, millega saab tudengit loengusse meelitada.
+        ['Loengupunktid', 21],
+        ['Hea õppejõud', 12],
+        ['Meemid/huvitav loeng', 10],
+        ['KT/eksami vastused', 3]
+    ], [//Nimeta toit, mida tudengid tarbivad.
+        ['Šokolaad/snickers', 16],
+        ['(kiir)nuudlid', 11],
+        ['Pelmeenid', 10],
+        ['Purgisupp', 3]
+    ], [//Nimeta üks avalikult tuntud TÜ õppejõud, kes sinu arvates saab kõige rohkem armastuskirju.
+        ['Tarkpea', 14],
+        ['Marek \"kõige parem\" Kolk', 9],
+        ['Mihhail \"Karumõmm Miša\" Lotman', 7],
+        ['Mats Mikkor', 1]
+    ], [//Nimeta üks põhjus, miks inimene tahab füüsikuks saada.
+        ['Keskmine palk', 15], // raha
+        ['Masohhistlikult vastupidav', 13],
+        ['Teadmisrõõm', 10],
+        ['Vereliin - pereliin', 7]
+    ], [//Nimeta üks asi, mille üle varas ei tahaks üllatuda, kui ta Füüsika Instituuti sisse murrab?
+        ['Kalev Tarkpea ootab koridoris', 30],
+        ['Tudengeid õppimas', 22],
+        ['STEM kaalub samapalju kui bemm', 11],
+        [' - ', 9]
+    ], [//Nimeta midagi, mida eeldatakse, et 25-aastane inimene on juba saavutanud.
+        ['Pesast välja hüpanud', 31],//Kodust välja kolinud
+        ['Elukaaslane/koduloom', 19],
+        ['Vumm-vumm', 9],//Auto
+        [' - ', 9]
+    ], [//Nimeta midagi, mida saab teha loodusteaduste bakalaureusekraadiga.
+        ['Magistrisse minna', 100],
+        ['-', 11],
+        ['-', 10],
+        [' - ', 9]
+    ]
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gameBlock[0] = [
+    [ // Milline on enim kasutatud programmeerimis-/skriptimis-/markupkeel (2018)
+        ["JavaScript", 70],
+        ["HTML", 69],
+        ["CSS", 65],
+        ["SQL", 57]
+    ],
+    [ // Kus õpid programmeerima?
+        ["Õpid ise", 99],
+        ["Ülikoolis", 70],
+        ["Tööl kursustel", 40],
+        ["Netikursustel", 20]
+    ],
+    [ // Milline on populaarseim operatsioonisüsteem? (2018)
+        ["Android", 40],
+        ["Windows", 36],
+        ["iOS", 13],
+        ["OS X", 5]
+    ],
+    [ // Kumb on parem, tabid või tühikud?
+        ["Tabid", 99],
+        ["Tühikud", 50],
+        ["Oleneb", 30],
+        ["Ei tea", 0]
+    ]
+];
+
+gameBlock[1] = [
+    [
+        ['M. Reemann', 32],
+        ['T. Jürgenstein', 25],
+        ['E. Paabo', 8],
+        ['P. Beier', 7]
+    ], [
+        ['M. Jõgiste', 16],
+        ['M. Piirimäe', 11],
+        ['E. Timak', 10],
+        ['L. Astover', 9]
+    ], [
+        ['U. Saar', 23],
+        ['Ü. Keerberg', 20],
+        ['A. Pettai', 14],
+        ['H. Köhler', 11]
+    ], [
+        ['K. Punga', 44],
+        ['O. Titova', 8],
+        ['J. Paaver', 7],
+        ['A. Saarva', 6]
+    ], [
+        ['H. Tering', 21],
+        ['H. Kiisel', 18],
+        ['L. Tepp', 11],
+        ['T. Pluum', 10]
+    ]
+];
+	</pre>
+
+	<h1>Küsimuste plokkide vahetamine</h1>
+	<ol>
+		<li>Kirjuta WinSCP-ga küsimuste JSON-i plokk üle (rename or sth)</li>
+		<li>Tee mängulaual FORCE refresh (CTRL + F5)</li>
+	</ol>
+
+
+	<h1>FÜS-i kevadpeole</h1>
+	<ul>
+		<li>Tee uus logo</li>
+		<li>Viska küsimused andmestruktuuri</li>
+		<li>Sebi omale kaugjuhitav klaviatuur ja teine arvuti (projekteerimiseks)</li>
+		<li>Sebi kõlarid ka!</li>
+	</ul>
+</body>
+</html>
